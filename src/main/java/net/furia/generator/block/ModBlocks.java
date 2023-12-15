ecc3b5df6c410c5c224f380acc83e0818330a044
LostQuasar <rionlion100@gmail.com>
2023-12-14 20:54:19 -0500

    Add to functional blocks item group

--- src/main/java/net/furia/generator/block/ModBlocks.java
+++ src/main/java/net/furia/generator/block/ModBlocks.java
@@ -1,10 +1,12 @@
 package net.furia.generator.block;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.furia.generator.GeneratorMod;
 import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -15,6 +17,9 @@ public class ModBlocks {
 
     public static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> {
+    	    content.add(block);
+        });
         return Registry.register(Registries.BLOCK, new Identifier(GeneratorMod.MOD_ID, name), block);
     }
 
